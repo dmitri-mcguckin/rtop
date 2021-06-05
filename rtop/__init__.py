e0123f021a6ff1e9d38431b5ebf3b540e95dce3e
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2021-06-05 17:42:23 -0400

    Add primitive curses app

--- rtop/__init__.py
+++ rtop/__init__.py
@@ -13,6 +13,19 @@ APP_VERSION = f'{MAJOR}.{MINOR}.{PATCH}'
 APP_LICENSE = 'GPL-3.0'
 APP_URL = 'https://github.com/dmitri-mcguckin/rtop'
 
-API_UPDATE_INTERVAL = dt.timedelta(seconds=10)
+API_UPDATE_INTERVAL = dt.timedelta(minutes=1)
+API_URI = 'https://fdo.rocketlaunch.live/json/launches/next/5'
+# API_URI = 'http://localhost:3000/api'
 
 CACHE_DIR = os.path.expanduser('~/.cache/rtop')
+
+
+def trunc_timedelta(time: dt.timedelta) -> str:
+    if(abs(int(time.total_seconds() / 86400)) > 1):
+        return f'{int(time.total_seconds() / 86400)}d'
+    elif(abs(int(time.total_seconds() / 3600)) > 1):
+        return f'{int(time.total_seconds() / 3600)}h'
+    elif(abs(int(time.total_seconds() / 60)) > 1):
+        return f'{int(time.total_seconds() / 60)}m'
+    else:
+        return f'{int(time.total_seconds())}s'
